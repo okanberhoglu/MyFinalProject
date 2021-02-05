e4ee50446fee382c9b50254b32b6681a75f4e05b
okan berhoğlu <okunber@gmail.com>
2021-02-05 23:46:44 +0300

    Yazılım kampı 05.02.21

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using Entities.Abstract;
+
+namespace DataAccess.Abstract
+{
+    //class : referans tip olabilir,
+    // IEntity : IEntity olabilir veya IEntity yi implemente eden bir nesne olabilir,
+    //new() : new lenebilir olmalı yani şuan IEntity olamaz sadece onu implemente eden nesne olabilir, T.
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
